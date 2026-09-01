c37ab1c5e439c7854134db70e3c0a839d66a3793
Amal almahmud <am9875073@gmail.com>
2026-09-01 22:50:41 +0300

    data setup for assePag

--- app/assessments/page.tsx
+++ app/assessments/page.tsx
@@ -1,3 +1,33 @@
-export default function assessments() {
-  return <main>assessments</main>;
+"use client"; // we need client components for localStorage, use effect, and useState
+
+//import "../assessments.css";
+import { useState, useEffect } from "react";
+
+type Course = {
+  id: string;
+  name: string;
+  difficulty: number;
+};
+
+
+type Assessment = {
+  id: string;
+  name: string;
+  courseId: string;
+  type: "Assignment" | "Quiz" | "Midterm" | "Final Exam" | "Project";
+  deadline: string; // ISO date string
+  
+};
+export default function AssessmentsPage() {
+  const [courseId, setCourseId] = useState("");
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [name, setName] = useState("");
+  const [type, setType] = useState("Assignment");
+  const [deadline, setDeadline] = useState("");
+
+   return (
+    <main>
+      <h1>My Assessments</h1>
+    </main>
+  );
 }
